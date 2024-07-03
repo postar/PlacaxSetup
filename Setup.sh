9a1072618b1234e3550a566c792b9180f8d01fe1
Mariano Tomás Pocztaruk <mpocztaruk_92@hotmail.com>
2024-07-03 20:39:45 -0300

    corrected setup

--- Setup.sh
+++ Setup.sh
@@ -1,4 +1,4 @@
 wget -P /home https://raw.githubusercontent.com/postar/PlacaxSetup/main/mesy-reports-setup.sh
-chmod +x /home/PlacaxReportsSetup.sh
+chmod +x /home/mesy-reports-setup.sh
 wget -P /home https://raw.githubusercontent.com/postar/PlacaxSetup/main/cloudflared.sh
 chmod +x /home/cloudflared.sh
